8c5885a1b22d50568912e6c02f7ec797cbe8c726
belaja-akacija <BA@pm.me>
2021-11-12 10:21:52 -0800

    added some debugging tools and tried some things

--- regref.sh
+++ regref.sh
@@ -3,6 +3,9 @@
 # Simple search in regex_reference database
 # Author: Belaja-akacija
 # V.1.0.2 -- Test build
+# TODO:
+# - Store category names in an array and allow tab completion for them when using -c flag
+# - Figure out how to not allow certain flags to be used together
 
 Category()
 {
@@ -17,7 +20,14 @@ Category()
 Item()
 {
         item=$OPTARG
-        result=$(grep --color=always -iE $item ~/Documents/scripts/regref/regref.txt )
+        result=$(grep --color=always -iE "$item" ~/Documents/scripts/regref/regref.txt )
+        echo $result
+}
+
+Regex()
+{
+        item=$OPTARG
+        result=$(grep --color=always -E "$item" ~/Documents/scripts/regref/regref.txt )
         echo $result
 }
 
@@ -29,27 +39,47 @@ List()
 
 Help()
 {
-        echo "\nWelcome to the RegEx Cheatsheet! (v.1.0.2)\nHere are your options"
+        echo "\nWelcome to the RegEx Cheatsheet! (v.1.0.3)\nHere are your options: "
         echo
+        echo "Usage: regref [OPTION]... [SEARCH]\nNOTE: for RegEx searches instead of keywords, use double quotes\n"
         echo "-l -- list all categories"
         echo "-c -- choose a category to view"
         echo "-i -- search for a specific item (case insensitive)"
+        echo "-I -- search for a specific item (case sensitive)"
+        echo "-v -- verbose mode, for debugging purposes"
         echo "-h -- this help menu"
         echo
 }
 
-while getopts "c:i:lh" flag
+
+while getopts "c:i:I:lhv" flag
 do
         case "${flag}" in
                 c)
                         Category
                         ;;
-                i) Item;;
+                i)
+                        #if [[$flag =~ $testing]];
+                        #then
+                        #Item
+                        #elif [[$flag !=~ $noIFlag]];
+                        #then
+                        #echo "Not possible."
+                        #exit
+                        #fi
+                        Item
+                        ;;
+                I)
+                        Regex
+                        ;;
+
+                #I) echo "yes";;
+
                 l) List;;
                 h) Help;;
+                v)
+                        set -xv;;
                 *) echo "Invalid option: -$flag"
                         exit;;
         esac
 done
-
-
