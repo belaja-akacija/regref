0a61b02f832a0aa9081b6fc62a46ff783277b69e
belaja-akacija <BA@pm.me>
2021-11-12 23:09:44 -0800

    more patches

--- bin/regref.sh
+++ bin/regref.sh
@@ -3,10 +3,6 @@
 # Simple search in regex_reference database
 # Author: Belaja-akacija
 # V.1.0.4a
-#
-# Idea: maybe get the pwd then find the files needed from there backwards?
-# but this assumes that the user is in the current working directory as the program... We'll see
-
 
 Category()
 {
@@ -38,15 +34,15 @@ List()
 
 Help()
 {
-        echo "\nWelcome to the RegEx Cheatsheet! (v.1.0.3)\nHere are your options: "
+        echo "\n\tWelcome to the RegEx Cheatsheet! (v.1.0.4a)"
         echo
-        echo "Usage: regref [OPTION]... [SEARCH]\nNOTE: for RegEx searches instead of keywords, use double quotes\n"
-        echo "-l -- list all categories"
-        echo "-c -- choose a category to view"
-        echo "-i -- search for a specific item (case insensitive)"
-        echo "-I -- search for a specific item (case sensitive)"
-        echo "-v -- verbose mode, for debugging purposes"
-        echo "-h -- this help menu"
+        echo "\tUsage: regref [OPTION]... [SEARCH]\n      NOTE: for RegEx searches instead of keywords, use double quotes\n"
+        echo "  +  -c   #-----------> choose a category to view"
+        echo "  +  -h   #-----------> this help menu"
+        echo "  +  -i   #-----------> search for a specific item (case insensitive)"
+        echo "  +  -I   #-----------> search for a specific item (case sensitive)"
+        echo "  +  -l   #-----------> list all categories"
+        echo "  +  -v   #-----------> verbose mode, for debugging purposes"
         echo
 }
 
@@ -68,9 +64,6 @@ do
                 h) Help;;
                 v)
                         set -xv;;
-                #shortlist)
-                        #echo c i I l h v
-                        #;;
                 *) echo "Invalid option: -$flag"
                         exit;;
         esac
